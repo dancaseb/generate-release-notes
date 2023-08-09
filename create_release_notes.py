3448485cb268d0fb226aaf46c47570ac64c6f021
dancaseb <danielisreeves@gmail.com>
2023-08-09 15:03:21 +0200

    add base branch input

--- create_release_notes.py
+++ create_release_notes.py
@@ -1,24 +1,21 @@
 import git
 import re
 
+
 class GitRepo:
     def __init__(self) -> None:
-       self.repo = git.Repo()
-    
-    # def update_source_json(self):
-    #     tags = self.get_tags()
-    #     for commit in self.repo.iter_commits(f"{tags[-2].commit}..{tags[-1].commit}"):
-    #         if commit == tags[-1].commit:
-    #             continue
+        self.repo = git.Repo()
 
     def get_changelog_diff(self):
-        diff = self.repo.git.diff("--unified=0", "HEAD^", "HEAD", "--", "CHANGELOG.md")
+        diff = self.repo.git.diff(
+            "--unified=0", "HEAD^", "HEAD", "--", "CHANGELOG.md")
         if not diff:
-            raise Exception("git diff --unified=0 HEAD^ HEAD -- CHANGELOG.md returned empty string. There is nothing to generate the release note from.")
+            raise Exception(
+                "git diff --unified=0 HEAD^ HEAD -- CHANGELOG.md returned empty string. There is nothing to generate the release note from.")
         return diff
 
-class ReleaseNoteGenerator:
 
+class ReleaseNoteGenerator:
     def __init__(self, diff) -> None:
         self.diff = diff
 
@@ -33,34 +30,40 @@ class ReleaseNoteGenerator:
         # extract date from release url
 
         changelog_lines = re.findall(extract_changes_pattern, diff, re.M)
-        parsed_content = {'release_headline': None, 'release_date': None, 'release_version' : None, 'repository_name': None, 'changes' : []}
+        parsed_content = {'release_headline': None, 'release_date': None,
+                          'release_version': None, 'repository_name': None, 'changes': []}
         for line in changelog_lines:
             if re.match(version_pattern, line):
-                parsed_content['release_headline'] = re.search(version_pattern, line).group(1)
-                parsed_content['release_version'] = re.search(version_pattern, line).group(2)
-                parsed_content['release_date'] = re.search(version_pattern, line).group(3)
+                parsed_content['release_headline'] = re.search(
+                    version_pattern, line).group(1)
+                parsed_content['release_version'] = re.search(
+                    version_pattern, line).group(2)
+                parsed_content['release_date'] = re.search(
+                    version_pattern, line).group(3)
             # e.g., ### Bug Fixes
             elif line.startswith('###'):
                 parsed_content['changes'].append({'change_headline': line})
             # commit messages
             elif line.startswith('*'):
                 parsed_content['changes'][-1]['change_description'] = line
-                parsed_content['changes'][-1]['commit_hash'] = re.search(commit_pattern, line).group(1)
-                    
+                parsed_content['changes'][-1]['commit_hash'] = re.search(
+                    commit_pattern, line).group(1)
+
         return parsed_content
-    
+
     def load_file(self, path):
         with open(path, 'r') as file:
             content = file.readlines()
         return content
 
     def write_file(self, path, content, parsed_diff):
-        pattern = r'^\<\!\-\-Release note v[0-9]+\.[0-9]+\.[0-9]+\!\-\-\>$'
+        delimeter_pattern = r'^\<\!\-\-Release note v[0-9]+\.[0-9]+\.[0-9]+\!\-\-\>$'
         with (open(path, 'w')) as file:
             found_delimiter = False
             for line in content:
-                if not found_delimiter and re.match(pattern, line):
-                    file.write(f"<!--Release note v{parsed_diff['release_version']}!-->\n")
+                if not found_delimiter and re.match(delimeter_pattern, line):
+                    file.write(
+                        f"<!--Release note v{parsed_diff['release_version']}!-->\n")
                     file.write(f"## {parsed_diff['release_headline']}\n\n")
                     for change in parsed_diff['changes']:
                         file.write(f"{change['change_headline']}\n\n")
@@ -75,9 +78,7 @@ class ReleaseNoteGenerator:
         print(parsed_diff)
         content = self.load_file('release-notes.md')
         self.write_file('release-notes.md', content, parsed_diff)
-        
-        
-    
+
 
 if __name__ == "__main__":
     Repo = GitRepo()
