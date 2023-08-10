6eb4b5741d1ad837233e15e79c729954f94c69c3
dancaseb <danielisreeves@gmail.com>
2023-08-10 13:55:29 +0200

    add source argument

--- create_release_notes.py
+++ create_release_notes.py
@@ -1,5 +1,6 @@
 import git
 import re
+import argparse
 
 
 class GitRepo:
@@ -11,35 +12,33 @@ class GitRepo:
             "--unified=0", "HEAD^", "HEAD", "--", "CHANGELOG.md")
         if not diff:
             raise Exception(
-                "git diff --unified=0 HEAD^ HEAD -- CHANGELOG.md returned empty string. There is nothing to generate the release note from.")
+                "git diff --unified=0 HEAD^ HEAD -- CHANGELOG.md returned empty string. There is nothing to generate the release note from!")
         return diff
 
 
 class ReleaseNoteGenerator:
-    def __init__(self, diff) -> None:
-        self.diff = diff
 
-    def parse_diff(self, diff):
+    def parse_diff(self, diff, source_repo):
 
         # match lines that start with a + and then a # or *. Plus sign is from the diff
         extract_changes_pattern = r'^\+([#*].*)'
         # match ## or ### followed by a space and then a version number in [] brackets
-        version_pattern = r'^#{2,3} (\[([0-9]+\.[0-9]+\.[0-9]+)\].* \(([0-9]{4}-[0-9]{2}-[0-9]{2})\))'
+        headline_pattern = r'^#{2,3} (\[([0-9]+\.[0-9]+\.[0-9]+)\].*) \(([0-9]{4}-[0-9]{2}-[0-9]{2})\)'
         # extract commit from github url
         commit_pattern = r'https:\/\/.*\/commit\/([0-9a-z]*)'
         # extract date from release url
 
         changelog_lines = re.findall(extract_changes_pattern, diff, re.M)
-        parsed_content = {'release_headline': None, 'release_date': None,
-                          'release_version': None, 'repository_name': None, 'changes': []}
+        parsed_content = {'compare_changes_url': None, 'release_date': None,
+                          'release_version': None, 'source_repo': source_repo, 'source_repo_url': f"https://github.com/{source_repo}", 'changes': []}
         for line in changelog_lines:
-            if re.match(version_pattern, line):
-                parsed_content['release_headline'] = re.search(
-                    version_pattern, line).group(1)
+            if re.match(headline_pattern, line):
+                parsed_content['compare_changes_url'] = re.search(
+                    headline_pattern, line).group(1)
                 parsed_content['release_version'] = re.search(
-                    version_pattern, line).group(2)
+                    headline_pattern, line).group(2)
                 parsed_content['release_date'] = re.search(
-                    version_pattern, line).group(3)
+                    headline_pattern, line).group(3)
             # e.g., ### Bug Fixes
             elif line.startswith('###'):
                 parsed_content['changes'].append({'change_headline': line})
@@ -60,14 +59,15 @@ class ReleaseNoteGenerator:
         # iterates file content and when finds delimeter_pattern, writes the release note and then continues writing the rest of the file
         delimeter_pattern = r'^\<\!\-\-Release note v[0-9]+\.[0-9]+\.[0-9]+\!\-\-\>$'
         with (open(path, 'w')) as file:
-            print(f"content of file is: {file_content}")
+            # print(f"content of file is: {file_content}")
             if not file_content:
                 # file is empty, write release note at the top
-                print('file is empty, writing release note at the top')
+                # print('file is empty, writing release note at the top')
                 file.write(f"# Changelog\n\n")
+                file.write("***\n")
                 self._write_release_notes(file, parsed_diff)
             else:
-                print('appending release note to existing file')
+                # print('appending release note to existing file')
                 found_delimiter = False
                 for line in file_content:
                     if not found_delimiter and re.match(delimeter_pattern, line):
@@ -80,21 +80,28 @@ class ReleaseNoteGenerator:
     def _write_release_notes(self, file, parsed_diff):
         file.write(
             f"<!--Release note v{parsed_diff['release_version']}!-->\n")
-        file.write(f"## {parsed_diff['release_headline']}\n\n")
+        file.write(f"### {parsed_diff['release_date']} [{parsed_diff['source_repo'].split('/')[1]}]({parsed_diff['source_repo_url']})\n")
+        file.write(f"* #### {parsed_diff['compare_changes_url']}\n\n")
         for change in parsed_diff['changes']:
             file.write(f"{change['change_headline']}\n\n")
             file.write(f"{change['change_description']}\n\n")
+        file.write("***\n")
 
-    def generate_release_note(self):
-        parsed_diff = self.parse_diff(self.diff)
-        # print(parsed_diff)
+    def generate_release_note(self, changelog_diff, source_repo):
+        parsed_diff = self.parse_diff(changelog_diff, source_repo)
+        print(parsed_diff)
         content = self.load_file('release-notes.md')
         self.write_file('release-notes.md', content, parsed_diff)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate release notes')
+    parser.add_argument(
+        '--source', type=str, help='Source repository to generate release notes from. Owner/repo format', required=True)
+    args = parser.parse_args()
+
     Repo = GitRepo()
     changelog_diff = Repo.get_changelog_diff()
 
-    ReleaseNote = ReleaseNoteGenerator(changelog_diff)
-    ReleaseNote.generate_release_note()
+    ReleaseNote = ReleaseNoteGenerator()
+    ReleaseNote.generate_release_note(changelog_diff, args.source)
