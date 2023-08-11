1d27171bc62fc97e7a47886c78feb56cafab2404
dancaseb <danielisreeves@gmail.com>
2023-08-11 11:02:38 +0200

    refactor
    verify parsed diff, move regexes

--- create_release_notes.py
+++ create_release_notes.py
@@ -1,10 +1,11 @@
 import git
 import re
 import argparse
+from regex_patterns import extract_changes_pattern, headline_pattern, commit_pattern, delimeter_pattern
 
 
 class GitRepo:
-    def __init__(self, path) -> None:
+    def __init__(self, path):
         # path to the git repo
         self.repo = git.Repo(path)
 
@@ -19,58 +20,48 @@ class GitRepo:
 
 class ReleaseNoteGenerator:
 
-    def parse_diff(self, diff, source_path):
-
-        # match lines that start with a + and then a # or *. Plus sign is from the diff
-        extract_changes_pattern = r'^\+([#*].*)'
-        # match ## or ### followed by a space and then a version number in [] brackets
-        headline_pattern = r'^#{2,3} (\[([0-9]+\.[0-9]+\.[0-9]+)\].*) \(([0-9]{4}-[0-9]{2}-[0-9]{2})\)'
-        # extract commit from github url
-        commit_pattern = r'https:\/\/.*\/commit\/([0-9a-z]*)'
-        # extract date from release url
-
-        source_repo = source_path.split('/')[-2] + '/' + source_path.split('/')[-1]
-        changelog_lines = re.findall(extract_changes_pattern, diff, re.M)
-        parsed_content = {'compare_changes_url': None, 'release_date': None,
+    def _parse_diff(self, diff, source_path):
+        source_repo = source_path.split(
+            '/')[-2] + '/' + source_path.split('/')[-1]
+        parsed_diff = {'compare_changes_url': None, 'release_date': None,
                           'release_version': None, 'source_repo': source_repo, 'source_repo_url': f"https://github.com/{source_repo}", 'changes': []}
+
+        changelog_lines = re.findall(extract_changes_pattern, diff, re.M)
         for line in changelog_lines:
             if re.match(headline_pattern, line):
-                parsed_content['compare_changes_url'] = re.search(
+                parsed_diff['compare_changes_url'] = re.search(
                     headline_pattern, line).group(1)
-                parsed_content['release_version'] = re.search(
+                parsed_diff['release_version'] = re.search(
                     headline_pattern, line).group(2)
-                parsed_content['release_date'] = re.search(
+                parsed_diff['release_date'] = re.search(
                     headline_pattern, line).group(3)
             # e.g., ### Bug Fixes
             elif line.startswith('###'):
-                parsed_content['changes'].append({'change_headline': line})
+                parsed_diff['changes'].append({'change_headline': line})
             # commit messages
             elif line.startswith('*'):
-                parsed_content['changes'][-1]['change_description'] = line
-                parsed_content['changes'][-1]['commit_hash'] = re.search(
+                parsed_diff['changes'][-1]['change_description'] = line
+                parsed_diff['changes'][-1]['commit_hash'] = re.search(
                     commit_pattern, line).group(1)
 
-        return parsed_content
+        self._verify_parsed_diff(parsed_diff)
+        return parsed_diff
 
-    def load_file(self, path):
+    def _load_file(self, path):
         print(path)
         with open(path, 'r') as file:
             content = file.readlines()
         return content
 
-    def write_file(self, path, file_content, parsed_diff):
+    def _write_file(self, path, file_content, parsed_diff):
         # iterates file content and when finds delimeter_pattern, writes the release note and then continues writing the rest of the file
-        delimeter_pattern = r'^\<\!\-\-Release note v[0-9]+\.[0-9]+\.[0-9]+\!\-\-\>$'
         with (open(path, 'w')) as file:
-            # print(f"content of file is: {file_content}")
             if not file_content:
                 # file is empty, write release note at the top
-                # print('file is empty, writing release note at the top')
                 file.write(f"# Changelog\n\n")
                 file.write("***\n")
                 self._write_release_notes(file, parsed_diff)
             else:
-                # print('appending release note to existing file')
                 found_delimiter = False
                 for line in file_content:
                     if not found_delimiter and re.match(delimeter_pattern, line):
@@ -91,22 +82,30 @@ class ReleaseNoteGenerator:
             file.write(f"{change['change_description']}\n\n")
         file.write("***\n")
 
-    def generate_release_note(self, changelog_diff, source_path, filename):
-        parsed_diff = self.parse_diff(changelog_diff, source_path)
+    def _verify_parsed_diff(self, parsed_diff):
+        for key, value in parsed_diff.items():
+            if value is None:
+                raise ValueError(
+                    f"Parsing diff failed. All values must be filled in. Value for key {key} is {value}.")
+
+    def generate_release_note(self, changelog_diff, source_repo_path, release_notes_path):
+        parsed_diff = self._parse_diff(changelog_diff, source_repo_path)
         print(parsed_diff)
-        content = self.load_file(filename)
-        self.write_file(filename, content, parsed_diff)
+        content = self._load_file(release_notes_path)
+        self._write_file(release_notes_path, content, parsed_diff)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate release notes')
     parser.add_argument(
-        '--source_path', type=str, help='Source repository to generate release notes from.', required=True)
-    parser.add_argument('--release_notes_path', type=str, help='File name to write release notes to.')
+        '--source_repo_path', type=str, help='Source repository path to generate release notes from.', required=True)
+    parser.add_argument('--release_notes_path', type=str,
+                        help='File name to write release notes to.', required=True)
     args = parser.parse_args()
 
-    Repo = GitRepo(args.source_path)
+    Repo = GitRepo(args.source_repo_path)
     changelog_diff = Repo.get_changelog_diff()
 
     ReleaseNote = ReleaseNoteGenerator()
-    ReleaseNote.generate_release_note(changelog_diff, args.source_path, args.release_notes_path)
+    ReleaseNote.generate_release_note(
+        changelog_diff, args.source_repo_path, args.release_notes_path)
