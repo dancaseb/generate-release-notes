07d28ec066c6df0092517a37cc70176b5162a7d0
dancaseb <danielisreeves@gmail.com>
2023-08-16 14:23:22 +0200

    Add REPO_RELEASE_URL

--- create_release_notes.py
+++ create_release_notes.py
@@ -8,28 +8,26 @@ class ReleaseNoteGenerator:
 
     def _parse_changelog(self):
         # date format YYYY-MM-DDTHH:MM:SSZ
-        parsed_changelog = {'compare_changes_url': None, 'release_date': os.environ['RELEASE_DATE'].split('T')[0],
+        parsed_changelog = {'release_url': os.environ['REPO_RELEASE_URL'],
+                            'release_date': os.environ['RELEASE_DATE'].split('T')[0],
                             'release_version': os.environ['TAG_NAME'], 'source_repo': os.environ['REPO_NAME'],
                             'source_repo_url': os.environ['REPO_URL'], 'changes': []}
 
         changelog_lines = [
             line for line in os.environ['CHANGELOG_BODY'].splitlines() if line]
         for line in changelog_lines:
-            if re.match(version_headline_pattern, line):
-                parsed_changelog['compare_changes_url'] = re.search(
-                    version_headline_pattern, line).group(1)
-            # e.g., ### Bug Fixes
-            elif line.startswith(change_headline_start):
+            # e.g. ### Bug Fixes. Do not match release headline
+            if line.startswith(change_headline_start) and not re.match(release_headline_pattern, line):
                 parsed_changelog['changes'].append(
                     {'change_headline': line.replace(change_headline_start, '', 1)})
-                # the changes are grouped under the headline, we will be modifying the last change headline group
-                parsed_changelog['changes'][-1]['details'] = []
-            # commit messages
+                # the commits are grouped under the changes headline
+                parsed_changelog['changes'][-1]['commits'] = []
+            # Add commit messages to the last change headline
             elif line.startswith(commit_message_start):
                 commit_message = line.replace(commit_message_start, '', 1)
                 commit_hash = re.search(commit_pattern, line).group(1)
-                parsed_changelog['changes'][-1]['details'].append(
-                    {'commit_message': commit_message, 'commit_hash': commit_hash})
+                parsed_changelog['changes'][-1]['commits'].append(
+                    {'message': commit_message, 'hash': commit_hash})
         return parsed_changelog
 
     def _load_file(self, path):
@@ -37,18 +35,18 @@ class ReleaseNoteGenerator:
             content = file.readlines()
         return content
 
-    def _prepend_release_note(self, path, file_content, parsed_diff):
-        # iterates file content and when finds delimeter_pattern, writes the release note and then continues writing the rest of the file
+    def _prepend_release_note(self, path, file_content, parsed_changelog):
+        # iterates file content and when finds delimeter_pattern, writes the release note. Then writes rest of the file
         with (open(path, 'w')) as file:
             if not file_content:
                 # file is empty, write release note at the top
                 self._write_release_note(
-                    file, parsed_diff, include_headline=True)
+                    file, parsed_changelog, include_headline=True)
             else:
                 found_delimiter = False
                 for line in file_content:
                     if not found_delimiter and re.match(delimeter_pattern, line):
-                        self._write_release_note(file, parsed_diff)
+                        self._write_release_note(file, parsed_changelog)
                         file.write(line)
                         found_delimiter = True
                     else:
@@ -56,42 +54,48 @@ class ReleaseNoteGenerator:
                 if not found_delimiter:
                     # delimeter_pattern not found in the file and file not empty, append release note
                     self._write_release_note(
-                        file, parsed_diff, include_headline=True)
+                        file, parsed_changelog, include_headline=True)
 
-    def _write_release_note(self, file, parsed_diff, include_headline=False):
+    def _write_release_note(self, file, parsed_changelog, include_headline=False):
         if include_headline:
             file.write(f"{file_headline}\n\n")
             file.write(f"{section_delimeter}\n")
-        file.write(f"<!--Release note {parsed_diff['release_version']}!-->\n")
         file.write(
-            f"### {parsed_diff['release_date']} [{parsed_diff['source_repo']}]({parsed_diff['source_repo_url']})\n")
-        file.write(f"* #### {parsed_diff['compare_changes_url']}\n\n")
-        for change in parsed_diff['changes']:
+            f"<!--Release note {parsed_changelog['release_version']}!-->\n")
+        file.write(
+            f"### {parsed_changelog['release_date']} [{parsed_changelog['source_repo']}]\
+            ({parsed_changelog['source_repo_url']})\n")
+        file.write(f"* #### {parsed_changelog['release_url']}\n\n")
+        for change in parsed_changelog['changes']:
             file.write(f"#### {change['change_headline']}\n\n")
-            for detail in change['details']:
-                file.write(f"* {detail['commit_message']}\n\n")
+            for commit in change['commits']:
+                file.write(f"* {commit['message']}\n\n")
         file.write(f"{section_delimeter}\n\n")
 
-    def _verify_parsed_diff(self, parsed_diff):
-        for key, value in parsed_diff.items():
+    def _verify_parsed_changelog(self, parsed_changelog):
+        for key, value in parsed_changelog.items():
             if not value:
                 raise ValueError(
-                    f"Parsing diff failed. All values must be filled in. Value for key '{key}' is missing.")
+                    f"Parsing changelog failed. All values must be filled in. Value for key '{key}' is missing.")
 
     def _output_env_variable(self, name, value):
         with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
             print(f'{name}={value}', file=output_file)
 
-    def generate(self, release_notes_path):
-        parsed_diff = self._parse_changelog()
-        self._verify_parsed_diff(parsed_diff)
+    def generate(self):
+        parsed_changelog = self._parse_changelog()
+        self._verify_parsed_changelog(parsed_changelog)
         # output REPO name and RELEASE version as env to be used later in workflow
         self._output_env_variable(
-            'REPO_RELEASE', f"{parsed_diff['source_repo']} {parsed_diff['release_version']}")
-        print(f"parsed diff: {parsed_diff}")
+            'REPO_NAME_RELEASE', f"{parsed_changelog['source_repo']} {parsed_changelog['release_version']}")
+        print(f"parsed changelog: {parsed_changelog}")
+
+        return parsed_changelog
+
+    def update_release_notes(self, parsed_changelog, release_notes_path):
         file_content = self._load_file(release_notes_path)
         self._prepend_release_note(
-            release_notes_path, file_content, parsed_diff)
+            release_notes_path, file_content, parsed_changelog)
 
 
 if __name__ == "__main__":
@@ -103,4 +107,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     ReleaseNote = ReleaseNoteGenerator()
-    ReleaseNote.generate(args.release_notes_path)
+    parsed_changelog = ReleaseNote.generate()
+    ReleaseNote.update_release_notes(parsed_changelog, args.release_notes_path)
--- patterns.py
+++ patterns.py
@@ -1,8 +1,6 @@
 # REGEX PATTERNS
-# match lines that start with a + and then a # or *. Plus sign is from the diff
-extract_changes_pattern = r'^\+([#*].*)'
-# match ## or ### followed by a space and then a version number in [] brackets
-version_headline_pattern = r'^#{2,3} (\[[0-9]+\.[0-9]+\.[0-9]+\].*) \([0-9]{4}-[0-9]{2}-[0-9]{2}\)'
+# release headline. Contains link to compare releases and date.
+release_headline_pattern = r'^#{2,3} (\[[0-9]+\.[0-9]+\.[0-9]+\].*) \([0-9]{4}-[0-9]{2}-[0-9]{2}\)'
 # extract commit from github url
 commit_pattern = r'https:\/\/.*\/commit\/([0-9a-z]*)'
 
@@ -15,4 +13,3 @@ change_headline_start = '### '
 # RELEASE NOTE PATTERNS
 file_headline = '# Changelog'
 section_delimeter = '***'
-
